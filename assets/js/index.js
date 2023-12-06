05faf18c47b436c880f6d22d5c2b382acb3cb1f2
Arastun123 <ekbr03@gmail.com>
2023-12-06 00:18:33 -0800

    Today page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,41 @@ $(".hamburger-menu").click(function () {
   $("#menu").slideToggle("active");
 });
 
-$(".dropdown-trigger").click ( function() {
-  $(".dropdown").fadeToggle()
-})
 
-$(window).scroll(function(){ 
-  let offset = $(window).scrollTop() + $(window).height(),
-  animatables = $('.animatable');
-  animatables.each(function() {
-    let animatable = $(this);
-    if ((animatable.offset().top + animatable.height() - 20) < offset) {
-      $("body").css({overflowX: 'hidden'});
-      animatable.removeClass('animatable').addClass('animated');
+$(() => {
+    $(".dropdown-trigger").click ( function() {
+    $(".dropdown").fadeToggle()
+  })
+
+  $(window).scroll(function(){ 
+    let offset = $(window).scrollTop() + $(window).height(),
+    animatables = $('.animatable');
+    animatables.each(function() {
+      let animatable = $(this);
+      if ((animatable.offset().top + animatable.height() - 20) < offset) {
+        $("body").css({overflowX: 'hidden'});
+        animatable.removeClass('animatable').addClass('animated');
+      }
+    });
+  })
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    center: true,
+    items: 4,
+    margin: 10,
+    autoplay: true,
+    dots: false,
+    nav: true,
+    autoplayTimeout: 1500,
+    smartSpeed: 450,
+    responsive: {
+        448: {
+            items: 2,
+        },
+        100: {
+            items: 1,
+        }
     }
   });
-})
+});
