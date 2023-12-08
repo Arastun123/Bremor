d7661b14bfb3db8da1c9c2e06ff46109aa92a0b3
Arastun123 <ekbr03@gmail.com>
2023-12-07 23:10:39 -0800

    Contact page

--- assets/js/catalogue.js
+++ assets/js/catalogue.js
@@ -7,3 +7,18 @@ $(".close-filter").click( function() {
     $("#selection-boxes").fadeOut("lazy");
     $(".open-filter").css("display", "block")
 })
+
+let item = $(".item")
+let defaultItem = 10
+let itemToShow = 5
+if (item.length > 10) { 
+    item.slice(defaultItem).hide() 
+
+    $("#more").click( function(){
+        item.slice(defaultItem, defaultItem + itemToShow).show();
+        defaultItem += itemToShow
+    })
+    if (defaultItem >= item.length) {
+        $(this).hide();
+    }
+}
