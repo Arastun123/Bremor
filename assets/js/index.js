e31e31008fea91b4f8c871f32dcac6b4de01f23b
Arastun123 <ekbr03@gmail.com>
2023-12-10 23:21:53 -0800

    Index  page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,41 +3,66 @@ $(".hamburger-menu").click(function () {
   $("#menu").slideToggle("active");
 });
 
+$(".dropdown-trigger").click ( function() {
+  $(".dropdown").fadeToggle()
+})
 
-$(() => {
-    $(".dropdown-trigger").click ( function() {
-    $(".dropdown").fadeToggle()
-  })
-
-  $(window).scroll(function(){ 
-    let offset = $(window).scrollTop() + $(window).height(),
-    animatables = $('.animatable');
-    animatables.each(function() {
-      let animatable = $(this);
-      if ((animatable.offset().top + animatable.height() - 20) < offset) {
-        $("body").css({overflowX: 'hidden'});
-        animatable.removeClass('animatable').addClass('animated');
-      }
-    });
-  })
-
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    center: true,
-    items: 4,
-    margin: 10,
-    autoplay: true,
-    dots: false,
-    nav: true,
-    autoplayTimeout: 1500,
-    smartSpeed: 450,
-    responsive: {
-        448: {
-            items: 2,
-        },
-        100: {
-            items: 1,
-        }
+$(window).scroll(function(){ 
+  let offset = $(window).scrollTop() + $(window).height(),
+  animatables = $('.animatable');
+  animatables.each(function() {
+    let animatable = $(this);
+    if ((animatable.offset().top + animatable.height() - 20) < offset) {
+      $("body").css({overflowX: 'hidden'});
+      animatable.removeClass('animatable').addClass('animated');
     }
   });
+})
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  center: true,
+  items: 4,
+  margin: 10,
+  autoplay: true,
+  dots: false,
+  nav: true,
+  autoplayTimeout: 1500,
+  smartSpeed: 450,
+  responsive: {
+      448: {
+          items: 2,
+      },
+      100: {
+          items: 1,
+      }
+  }
+});
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) { showSlides((slideIndex += n)) }
+
+function currentSlide(n) { showSlides((slideIndex = n)) }
+
+function showSlides(n) {
+  let i;
+  let slides = $(".slider-item");
+  let dots = $(".dot");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  slides.hide();
+  dots.removeClass("active");
+
+  slides.eq(slideIndex - 1).show();
+  dots.eq(slideIndex - 1).addClass("active");
+}
+
+$(".dot").on("click", function() {
+  let dotIndex = $(this).index() + 1;
+  currentSlide(dotIndex);
 });
