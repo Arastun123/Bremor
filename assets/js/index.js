d3e0a6504eabdff7c8ac4f80be87ea865689e24d
Arastun123 <ekbr03@gmail.com>
2023-12-12 01:10:56 -0800

    Desing

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,17 @@ $(".hamburger-menu").click(function () {
   $("#menu").slideToggle("active");
 });
 
-$(".dropdown-trigger").click ( function() {
+$(".dropdown-trigger").click(function () {
   $(".dropdown").fadeToggle()
 })
 
-$(window).scroll(function(){ 
+$(window).scroll(function () {
   let offset = $(window).scrollTop() + $(window).height(),
-  animatables = $('.animatable');
-  animatables.each(function() {
+    animatables = $('.animatable');
+  animatables.each(function () {
     let animatable = $(this);
     if ((animatable.offset().top + animatable.height() - 20) < offset) {
-      $("body").css({overflowX: 'hidden'});
+      $("body").css({ overflowX: 'hidden' });
       animatable.removeClass('animatable').addClass('animated');
     }
   });
@@ -27,19 +27,29 @@ $(".owl-carousel").owlCarousel({
   autoplay: true,
   dots: false,
   nav: true,
-  autoplayTimeout: 1500,
+  autoplayTimeout: 3000,
   smartSpeed: 450,
   responsive: {
-      768: {
-        items: 2,
-      },
-      448: {
-        items: 1,
-      },
-      100: {
-        items: 1,
-      }
+    768: {
+      items: 2,
+    },
+    448: {
+      items: 1,
+    },
+    100: {
+      items: 1,
+    }
   }
+});
+
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 7000,
+  cssEase: 'linear',
 });
 
 
@@ -65,11 +75,11 @@ function showSlides(n) {
   dots.eq(slideIndex - 1).addClass("active");
 }
 
-$(".dot").on("click", function() {
+$(".dot").on("click", function () {
   let dotIndex = $(this).index() + 1;
   currentSlide(dotIndex);
 });
 
-$("#scrollToTop").click( function(){
-  $("html, body").animate({ scrollTop: 0  }, "slow");
+$("#scrollToTop").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
 })
